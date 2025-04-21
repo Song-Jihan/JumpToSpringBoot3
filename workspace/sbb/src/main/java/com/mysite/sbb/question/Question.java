a3caf8f67cb53fe5f952664c01551931484fcca5
송지한 <157107006+Song-Jihan@users.noreply.github.com>
2025-04-21 19:02:43 +0900

    점프 투 스프링부트3 ~2-11

---
+++ workspace/sbb/src/main/java/com/mysite/sbb/question/Question.java
@@ -0,0 +1,37 @@
+package com.mysite.sbb.question;
+
+import java.util.List;
+
+import com.mysite.sbb.answer.Answer;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
+import lombok.Setter;
+import lombok.Getter;
+
+@Setter
+@Getter
+@Entity
+public class Question {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length=200)
+	private String subject;
+	
+	@Column(columnDefinition="TEXT")
+	private String content;
+	
+	private LocalDateTime createDate;
+	
+	@OneToMany(mappedBy="question",cascade=CascadeType.REMOVE)
+	private List<Answer> answerList;
+	
+}
