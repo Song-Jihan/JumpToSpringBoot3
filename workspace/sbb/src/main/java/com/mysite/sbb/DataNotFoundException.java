6beb8e92492feaa21c8df6ba5e4eb8625fbfe815
송지한 <157107006+Song-Jihan@users.noreply.github.com>
2025-04-23 00:13:19 +0900

    점프 투 스프링부트3 ~2-14

--- workspace/sbb/src/main/java/com/mysite/sbb/DataNotFoundException.java
+++ workspace/sbb/src/main/java/com/mysite/sbb/DataNotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value=HttpStatus.NOT_FOUND,reason="entity not found")
 public class DataNotFoundException extends RuntimeException{
-	private static final long serialVersion=1;
+	private static final long serialVersion=1L;
 	public DataNotFoundException(String message) {
 		super(message);
 	}
