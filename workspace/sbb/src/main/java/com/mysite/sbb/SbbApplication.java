ea327e636e764c81f4c67c49f3cb595f07d04af6
송지한 <157107006+Song-Jihan@users.noreply.github.com>
2025-04-25 01:42:12 +0900

    점프 투 스프링부트3 ~2-16

--- workspace/sbb/src/main/java/com/mysite/sbb/SbbApplication.java
+++ workspace/sbb/src/main/java/com/mysite/sbb/SbbApplication.java
@@ -1,5 +1,7 @@
 package com.mysite.sbb;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
