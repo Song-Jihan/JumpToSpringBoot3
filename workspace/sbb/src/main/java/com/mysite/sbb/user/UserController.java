3ba59dfb8b94f51a86070f426e974cabc03dec80
송지한 <157107006+Song-Jihan@users.noreply.github.com>
2025-05-01 01:49:36 +0900

    점프 투 스프링부트3 ~3-06
    책에는 없는 추가기능
    - 문제점: 회원가입 페이지에서 빈칸으로 입력했을 경우, 나타나는 경고창의 순서가 매번 페이지를 불러올때마다 랜덤한 순서로 출력됨.
    - 원인: form_errors.html의 #fields.hasAllErrors()는 순서를 보장하지 않고 경고창을 마구잡이로 가져옴.
    - 해결: 회원가입 페이지의 경고창을 위한 signup_form_errors.html을 별도로 생성하고, usercontroller에서 원하는 필드의 순서를 지정.

---
+++ workspace/sbb/src/main/java/com/mysite/sbb/user/UserController.java
@@ -0,0 +1,76 @@
+package com.mysite.sbb.user;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	private final UserService userService;
+	
+	@GetMapping("/signup")
+	public String signup(UserCreateForm userCreateForm) {
+		return "signup_form";
+	}
+	
+	@PostMapping("/signup")
+	public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult,Model model) {
+		
+		List<String> fieldOrder = List.of("userName", "password1", "password2", "email");
+
+		List<String> orderedErrors = new ArrayList<>();
+
+		for (String field : fieldOrder) {
+		    List<FieldError> fieldErrors = bindingResult.getFieldErrors(field);
+		    for (FieldError error : fieldErrors) {
+		        orderedErrors.add(error.getDefaultMessage());
+		    }
+		}
+		
+		if (!orderedErrors.isEmpty()) {
+	        model.addAttribute("orderedErrors", orderedErrors);
+	        return "signup_form";
+	    }
+		
+		if(!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+			bindingResult.rejectValue("password2","passwordInCorrect","2개의 패스워드가 일치하지 않습니다.");
+			return "signup_form";
+		}
+		
+		try {
+			userService.create(userCreateForm.getUserName(), userCreateForm.getPassword1(), userCreateForm.getEmail());
+		}
+		catch(DataIntegrityViolationException e) {
+			e.printStackTrace();
+			bindingResult.reject("signupFailed","이미 등록된 사용자입니다.");
+			return "signup_form";
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			bindingResult.reject("signupFailed",e.getMessage());
+			return "signup_form";
+		}
+		
+		if(bindingResult.hasErrors()) {
+			orderedErrors.clear();
+			return "signup_form";
+		}
+		
+		
+		
+		return "redirect:/";
+	}
+}
